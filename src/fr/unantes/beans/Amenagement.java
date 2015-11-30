7ec5068858aca87b78a4272ea52a8b1a57976b62
Gueoff <geoffrey.desbrosses@gmail.com>
2015-11-30 22:42:11 +0100

    premier commit

---
+++ src/fr/unantes/beans/Amenagement.java
@@ -0,0 +1,50 @@
+package fr.unantes.beans;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+@Entity
+public class Amenagement {
+
+	@Id
+	private Long id;
+	private String nom;
+	private String description;
+	public Amenagement() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Amenagement(Long id, String nom, String description) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.description = description;
+	}
+	
+	
+	public Amenagement(String nom, String description) {
+		super();
+		this.nom = nom;
+		this.description = description;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
